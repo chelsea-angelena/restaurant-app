434865bdd22b622949ff5a3a9fbc24e83df01cd1
Chelsea Angelena <chelsea.angelena@gmail.com>
2020-11-09 17:49:17 -0800

    updated logo

--- app.config.js
+++ app.config.js
@@ -7,8 +7,10 @@ const YELP_API_KEY = process.env.EXPO_YELP_API_KEY;
 
 export default {
 	name: 'restaurantsearch',
+	icon: './assets/foodLogo.jpg',
 	slug: 'restaurantsearch',
-	// version: process.env.DEV || '1.0.0',
+	description:
+		'This is a mobile app built using React-Native and Expo. Search for nearby restaurants. Results are determined by the type of dish or business name specified, and then filtered into 4 different price catgories. Each listed result has detailed information such as location, hours and photographs. ',
 	extra: {
 		apiKey: process.env.EXPO_YELP_API_KEY,
 	},
