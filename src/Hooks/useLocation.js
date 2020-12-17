84d59cd7593fb2269f9335b639affba6bd9ee92f
Chelsea Angelena <chelsea.angelena@gmail.com>
2020-12-17 01:18:21 -0800

    location re-enabled

--- src/Hooks/useLocation.js
+++ src/Hooks/useLocation.js
@@ -1,42 +1,42 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-// import * as Location from 'expo-location';
+import * as Location from 'expo-location';
 
-// export default function useLocation() {
-// 	const [latitude, setLatitude] = useState({});
-// 	const [longitude, setLongitude] = useState({});
-// 	const [loading, setLoading] = useState(true);
+export default function useLocation() {
+	const [latitude, setLatitude] = useState({});
+	const [longitude, setLongitude] = useState({});
+	const [loading, setLoading] = useState(true);
 
-// 	// useEffect(() => {
-// 	// 	(async () => {
-// 	// 		let { status } = await Location.requestPermissionsAsync();
-// 	// 		if (status !== 'granted') {
-// 	// 			setErrorMsg('Permission to access location was denied');
-// 	// 		}
-// 	// 		let result = await Location.getCurrentPositionAsync({});
-// 	// 		setLocation(result);
-// 	// 	})();
-// 	// 	setLoading(false);
-// 	// }, []);
+	// useEffect(() => {
+	// 	(async () => {
+	// 		let { status } = await Location.requestPermissionsAsync();
+	// 		if (status !== 'granted') {
+	// 			setErrorMsg('Permission to access location was denied');
+	// 		}
+	// 		let result = await Location.getCurrentPositionAsync({});
+	// 		setLocation(result);
+	// 	})();
+	// 	setLoading(false);
+	// }, []);
 
-// 	useEffect(() => {
-// 		(async () => {
-// 			let { status } = await Location.requestPermissionsAsync();
-// 			if (status !== 'granted') {
-// 				setErrorMsg('Permission to access location was denied');
-// 			}
-// 			let location = await Location.getCurrentPositionAsync({});
-// 			setLongitude(location.coords.longitude);
-// 			setLatitude(location.coords.latitude);
-// 		})();
-// 		setLoading(false);
-// 	}, []);
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestPermissionsAsync();
+			if (status !== 'granted') {
+				setErrorMsg('Permission to access location was denied');
+			}
+			let location = await Location.getCurrentPositionAsync({});
+			setLongitude(location.coords.longitude);
+			setLatitude(location.coords.latitude);
+		})();
+		setLoading(false);
+	}, []);
 
-// 	// if (errorMsg) {
-// 	// 	return 'errorMsg';
-// 	// } else if (location) {
-// 	// 	return JSON.stringify(location);
-// 	// }
+	// if (errorMsg) {
+	// 	return 'errorMsg';
+	// } else if (location) {
+	// 	return JSON.stringify(location);
+	// }
 
-// 	return [latitude, longitude];
-// }
+	return [latitude, longitude];
+}
--- src/Hooks/useResults.js
+++ src/Hooks/useResults.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect, useContext } from 'react';
 import yelp from '../api/yelp';
+import useLocation from './useLocation';
 
 export default function useResults() {
 	const [results, setResults] = useState([]);
 	const [error, setError] = useState(null);
-
+	const [latitude, longitude] = useLocation();
 	const searchApi = async (searchTerm) => {
+		console.log(latitude, longitude);
 		try {
 			const response = await yelp.get('/search', {
 				params: {
 					limit: 50,
 					term: searchTerm,
-					location: 'Victoria, British Columbia',
+					latitude: latitude,
+					longitude: longitude,
 				},
 			});
 			setResults(response.data.businesses);
